0161f82b268525133f43b9148d130066ae9419ee
chaitanya <newarechaitanya832@gmail.com>
2024-12-29 14:50:24 +0530

    sidebar and header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+"use client"
+
+import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from "@clerk/nextjs"
 
 const Header = () => {
+    const {user}=useUser()
   return (
-    <div>
+    <div className="flex items-center justify-between px-6 py-4 border-b">
+        {user &&(
+            <h1 className="text-2xl">
+                {user?.firstName}
+                {`'s`} Space
+            </h1>
+        )}
+
+        <div>
+            <SignedOut>
+                <SignInButton/>
+            </SignedOut>
+
+            <SignedIn>
+                <UserButton/>
+            </SignedIn>
+        </div>
       
     </div>
   )
---
+++ components/NewDocumentButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./ui/button"
+
+
+const NewDocumentButton = () => {
+  return (
+    <div>
+      <Button>Create new</Button>
+    </div>
+  )
+}
+
+export default NewDocumentButton
